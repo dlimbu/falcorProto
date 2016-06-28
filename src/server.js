ba56869ad689b9e3b8b8f98454eb258660ce9a65
dipu limbu <dlimbu@dipus-Mac-mini.local>
2016-06-28 14:01:28 -0600

    node modules and falcor-router

---
+++ src/server.js
@@ -0,0 +1,13 @@
+
+var express = require('express');
+
+var inst = express();
+
+inst.get('/', function (req, res) {
+  res.send("Hello World");
+});
+
+inst.listen(8080, function () {
+   console.log("Falcore server started");
+});
+
