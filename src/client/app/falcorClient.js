94d0963b961d1bed98578ddacf97628d2a507756
dlimbu <dipu.limbu@twcable.com>
2016-06-29 16:49:28 -0600

    cp

--- src/client/app/falcorClient.js
+++ src/client/app/falcorClient.js
@@ -6,10 +6,12 @@ define(function (require, exports, module) {
 
    var Falcor = require('falcor');
 
+   var view = document.getElementById("view");
    var serverPath = "http://localhost:8080";
 
    var FalcorTest = function () {
       this.createModel();
+
    };
 
    FalcorTest.prototype.createModel = function () {
@@ -18,11 +20,28 @@ define(function (require, exports, module) {
       });
    };
 
-   FalcorTest.prototype.sendReq = function () {
-      document.write("FalcorClient sending req !!!! ");
-      this.model.get("id").then(function (resp) {
-         document.write("\nFalcor server response " + resp.json.id);
-      });
+   var createTextElem = function (text) {
+      var pfText = document.createElement("Pre");
+      var tn =  document.createTextNode(text);
+      pfText.appendChild(tn);
+      return pfText;
+   };
+
+   FalcorTest.prototype.getOndemand = function (afrom, ato, value) {
+
+      var odPathArray = ["ondemand", "action",
+         {
+            from: afrom,
+            to: ato
+         },
+         value];
+
+      this.model.get(odPathArray).then(function(resp) {
+         var pathArray = JSON.stringify(odPathArray, null, '  ');
+         var respStr = JSON.stringify(resp, null, '  ');
+         var input = "Request.Path: "+ pathArray + "\n\n Response: " + respStr;
+         view.appendChild(createTextElem(input));
+      })
    };
 
    exports.FalcorTest = FalcorTest;
--- src/client/lib/main.js
+++ src/client/lib/main.js
@@ -12,6 +12,22 @@ requirejs.config({
 
 requirejs(['app/falcorClient'], function (falcorClient) {
    var fT = new falcorClient.FalcorTest();
-   fT.sendReq();
+
+   /**
+    * All the requests returns the whole data set
+    * instead of the subset request based on our targeted
+    * request.
+    */
+
+   // (from, to, value);
+   fT.getOndemand(0, 2, "title");
+   fT.getOndemand(0, 4, "title");
+   fT.getOndemand(1, 4, "title");
+
+   /**
+    * tmsId makes a seperate request even though the
+    * data is cache.
+    */
+   fT.getOndemand(1, 3, "tmsId");
 });
 
